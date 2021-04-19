c778432a32e4c54a961f337a1fce16b349bfb7ed
Omar Branez <omar.branez@gmail.com>
2021-04-19 11:51:50 -0400

    commit tree 96c25792cf5320c81b72669860353ef218087be4

--- app.rb
+++ app.rb
@@ -6,11 +6,18 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
+    puts params # it's in the console down below!
+    # original_string = params["string"]
+    # @reversed_string = original_string.reverse
+    @reversed_string = (params["string"]).reverse
     erb :reversed
   end
 
   get '/friends' do
     # Write your code here!
+    @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
+    
+    erb :friends
 
   end
 end
